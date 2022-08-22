9f9da0cad7c445cc6cb852dbdd21d859373795fc
SDVoGo <abirettoni@gmail.com>
2022-08-22 14:57:23 +0200

    Completa implementazione di lettura tramite Datatable e modifica del log con barre di cariacamento

---
+++ lib/utils.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from pathlib import Path
+from random import choices, randrange
+import numpy as np
+from rich import print
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent
+
+def flat_to_dict(lst:list):
+    """
+    Crea una lista 2D da una multilivello.
+    input lst:lista
+    """
+    if lst == []:
+        return lst
+    if isinstance(lst[0], list):
+        return flat_to_dict(lst[0]) + flat_to_dict(lst[1:])
+    return lst[:1] + flat_to_dict(lst[1:])
+
+def get_righe(lst):
+    return lst["righe"]
+
+def print_log(messaggio:str, rewritable:bool=False):
+    sys_log_orario = datetime.now()
+    msg = f"[[white]{sys_log_orario.hour:02d}:{sys_log_orario.minute:02d}:{sys_log_orario.second:02d}[/white]]: {messaggio}"
+    print(msg)
+
+# Se la deviazione rilascia valori negativi. Perchè non pulirli dall'interno?
+def distribuzione_normale(mean, sigma, num_valori_richiesti:int=1):
+    """
+    Calcola la distribuzione normale per creare rumore nei dati mantendo la verosomiglianza.
+    ATTENZIONE: viene effettuato un round interno e risoluzione problema numero negativo.
+    """
+    s = np.random.normal(mean, sigma, num_valori_richiesti)
+    res = []
+    for item in s:
+        while round(item) <= 0:
+            item = np.random.normal(mean, sigma,)
+        res.append(round(item))
+    #if len(res) == 1: return res[0]
+    return res
+
+def random_sec(data_in_stringa):
+    """
+    Randomizza i secondi, pensata per la data di consolidamento
+    """
+    temp_sec = str(randrange(0,60))
+    if len(temp_sec) == 1:
+        temp_sec = "0" + temp_sec
+    return f"{data_in_stringa}:{temp_sec}"
+
+def straordinari():
+    """
+    Sceglie se questo straordinario s'ha da fare.
+    """
+    return choices(population=[True, False], weights=(1,3), k=1)[0]
--- miniMoover.py
+++ miniMoover.py
@@ -1,75 +1,19 @@
 from collections import OrderedDict
+from rich import print
+import rich.progress as pro
 from datetime import datetime, timedelta
-from random import choices, randrange
+from random import randrange
 import sys
 import pandas as pd
 import numpy as np
 import os
-from colorama import init, Fore, Back, Style
 from uuid import uuid4
 import datatable as dt
 from shelve import open
+from lib.utils import straordinari, distribuzione_normale, flat_to_dict, get_project_root as root, print_log
+from pathlib import Path
 
 campi_insert = ["DITTA","DEPOSITO","CODMACCHINA","STAZIONE","NUMREG","PROGRIGA","CODARTICOLO","VARIANTE","STARTDATE","ENDDATE","QTAORDINE","TEMPO","TEMPOPEZZO","CONTAPEZZI","CODFASE","CODREPARTO","CODOPERATORE","STATOMACCHINA","DATACONSOLIDAMENTO"]
-
-def flat_to_dict(lst:list):
-    """
-    Crea una lista 2D da una multilivello.
-    input lst:lista
-    """
-    if lst == []:
-        return lst
-    if isinstance(lst[0], list):
-        return flat_to_dict(lst[0]) + flat_to_dict(lst[1:])
-    return lst[:1] + flat_to_dict(lst[1:])
-
-def get_righe(lst):
-    return lst["righe"]
-
-
-def print_log(messaggio:str,end="\n", rewritable:bool=False, clear:bool=True):
-    """
-    Variante custom basata sulla funzione built-in 'print'. 
-    (messaggio:str, end="\n", rewritable:bool=False,clear:bool=True) => str '[HH:MM:SS]: messaggio {end}'
-    """
-    sys_log_orario = datetime.now()
-    msg = f"[{sys_log_orario.hour:02d}:{sys_log_orario.minute:02d}:{sys_log_orario.second:02d}]: {messaggio}"
-    CLEAN_LINE = "                                                           "
-    if clear : print(CLEAN_LINE,end="\r")
-    if rewritable:
-        print(msg, end="\r")
-    elif not rewritable and bool(end):
-        print(msg, end=end)
-
-# Se la deviazione rilascia valori negativi. Perchè non pulirli dall'interno?
-def distribuzione_normale(mean, sigma, num_valori_richiesti:int=1):
-    """
-    Calcola la distribuzione normale per creare rumore nei dati mantendo la verosomiglianza.
-    ATTENZIONE: viene effettuato un round interno e risoluzione problema numero negativo.
-    """
-    s = np.random.normal(mean, sigma, num_valori_richiesti)
-    res = []
-    for item in s:
-        while round(item) <= 0:
-            item = np.random.normal(mean, sigma,)
-        res.append(round(item))
-    #if len(res) == 1: return res[0]
-    return res
-
-def random_sec(data_in_stringa):
-    """
-    Randomizza i secondi, pensata per la data di consolidamento
-    """
-    temp_sec = str(randrange(0,60))
-    if len(temp_sec) == 1:
-        temp_sec = "0" + temp_sec
-    return f"{data_in_stringa}:{temp_sec}"
-
-def straordinari():
-    """
-    Sceglie se questo straordinario s'ha da fare.
-    """
-    return choices(population=[True, False], weights=(1,3), k=1)[0]
 
 # Elaborazione giornata lavorativa
 def elabora_giornata(orario:list, lista_articoli_possibili_macchina, macchina):
@@ -154,17 +98,17 @@ def creazione_lista_ordini_per_macchina(macchina, lista_articoli_possibili_full)
     """
     try:
         if not bool(macchina["INIZIO LOG"]):
-            print_log(f"{Fore.RED}[ERRORE] La macchina {macchina['CODMACCHINA']} non ha la data di inizio elaborazione.\nImpossibile creare il log.{Style.RESET_ALL}")
+            print_log(f"[red][ERRORE] La macchina {macchina['CODMACCHINA']} non ha la data di inizio elaborazione.\nImpossibile creare il log.[/red]")
             return
         data_inizio = datetime.strptime(macchina["INIZIO LOG"], "%d/%m/%Y")
         # Se data fine non è settata ma data inizio sì, significa che deve elaborare fino ad oggi
-        if bool(macchina["FINE LOG"]):
+        if not bool(macchina["FINE LOG"]):
             data_fine = datetime.today()
         else:
             data_fine =  datetime.strptime(macchina["FINE LOG"], "%d/%m/%Y")
         codmacchina = macchina["CODMACCHINA"]
     except:
-        sys.exit(f"{Fore.RED}[ERRORE]: Riscontrato un problema in fase di lettura. CODMACCHINA: {codmacchina}{Style.RESET_ALL}")
+        sys.exit(f"[red][ERRORE]: Riscontrato un problema in fase di lettura. CODMACCHINA: {codmacchina}[/red]")
 
     data_in_elaborazione = data_inizio
     print_log(f"Estrapolazione possibili articoli lavorabili da {codmacchina}.")
@@ -172,34 +116,44 @@ def creazione_lista_ordini_per_macchina(macchina, lista_articoli_possibili_full)
 
     print_log(f"Avvio operazione su LOG per {codmacchina}.")
     # Inizio ciclo Giornate - finichè non ho record per tutti i giorni nel range
-    lista_ordini = []
-    while(data_in_elaborazione <= data_fine):
-        # Identifica orari in base al giorno della settimana
-        match data_in_elaborazione.isoweekday():
-            # Dom - salta l'elaborazione
-            case 7:
-                pass
-            # Sab - imposta turni da straordinario
-            case 6:
-                mattina_inizio = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=6 )
-                mattina_fine = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=13 )
-                orario = [(mattina_inizio, mattina_fine)]
-                print_log(f"Elaborazione data {data_in_elaborazione.day:02d}/{data_in_elaborazione.month:02d}/{data_in_elaborazione.year}.",rewritable=True, clear=False)
-                lista_ordini.append(elabora_giornata(orario, lista_articoli_possibili_macchina, macchina))
-            # Lun - Ven - imposta orari standard
-            case _:
-                mattina_inizio = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=8 )
-                mattina_fine = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=12, minute=30 )
-                pomeriggio_inizio = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=14 )
-                pomeriggio_fine = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=17, minute=45 )
-                orario = [(mattina_inizio, mattina_fine),(pomeriggio_inizio, pomeriggio_fine)]
-                print_log(f"Elaborazione data {data_in_elaborazione.day:02d}/{data_in_elaborazione.month:02d}/{data_in_elaborazione.year}.", rewritable=True, clear=False)
-                lista_ordini.append(elabora_giornata(orario, lista_articoli_possibili_macchina, macchina))
+    with pro.Progress(pro.SpinnerColumn(style="bold rgb(1,185,255)"),
+                    pro.TextColumn("{task.description}",style="bold rgb(1,185,255)",),
+                    pro.BarColumn(style="dim rgb(1,185,255)",complete_style="bold rgb(1,185,255)", finished_style="bright_green"),
+                    pro.MofNCompleteColumn(),
+                    pro.TimeElapsedColumn(),
+                    refresh_per_second=60) as progressbar:
+        task = progressbar.add_task("In esecuzione...", total=(data_fine-data_inizio).days)
         
+        lista_ordini = []
+        while(data_in_elaborazione <= data_fine):
+            # Identifica orari in base al giorno della settimana
+            match data_in_elaborazione.isoweekday():
+                # Dom - salta l'elaborazione
+                case 7:
+                    pass
+                # Sab - imposta turni da straordinario
+                case 6:
+                    if straordinari():
+                        mattina_inizio = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=6 )
+                        mattina_fine = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=13 )
+                        orario = [(mattina_inizio, mattina_fine)]
+                        # print_log(f"Elaborazione data {data_in_elaborazione.day:02d}/{data_in_elaborazione.month:02d}/{data_in_elaborazione.year}.",rewritable=True)
+                        lista_ordini.append(elabora_giornata(orario, lista_articoli_possibili_macchina, macchina))
+                # Lun - Ven - imposta orari standard
+                case _:
+                    mattina_inizio = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=8 )
+                    mattina_fine = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=12, minute=30 )
+                    pomeriggio_inizio = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=14 )
+                    pomeriggio_fine = datetime(day=data_in_elaborazione.day, month=data_in_elaborazione.month, year=data_in_elaborazione.year, hour=17, minute=45 )
+                    orario = [(mattina_inizio, mattina_fine),(pomeriggio_inizio, pomeriggio_fine)]
+                    # print_log(f"Elaborazione data {data_in_elaborazione.day:02d}/{data_in_elaborazione.month:02d}/{data_in_elaborazione.year}.", rewritable=True)
+                    lista_ordini.append(elabora_giornata(orario, lista_articoli_possibili_macchina, macchina))
 
-        # Avanzamento ciclo giornate
-        data_in_elaborazione += timedelta(days=1)
-    print_log(f"{Back.GREEN} COMPLETATO {Style.RESET_ALL}")
+
+            # Avanzamento ciclo giornate
+            data_in_elaborazione += timedelta(days=1)
+            progressbar.update(task, advance=1)
+        progressbar.update(task, description="[bright_green]Completato!")
     return lista_ordini
 
 def to_desktop(nome:str="test.txt"):
@@ -216,28 +170,36 @@ def get_var(name_key):
     with open("test-shelf", flag = 'c', protocol=None, writeback = False) as shelf:
         return shelf[name_key]
 
-
-# Inizializzazione colori
-init()
 # Lettura file in DATA
 print_log("Avvio script...")
 script_start_time = datetime.now()
 print_log("Lettura dei files nella cartella 'data'.")
 print_log("Lettura file 'lista macchine.csv'.")
 # Lettura "lista macchine.csv"
+# try:
+# Percorso per la root del progetto. Pyinstaller fa casino altrimenti
+global data_path
+data_path = Path(f"{root()}/data")
+# Pyinstaller fa casino con la versione EXE
 try:
-    df_lista_macchine = pd.read_csv("data/lista macchine.csv")
+    try:
+        df_lista_macchine = dt.fread(f"{data_path}/lista macchine.csv").to_pandas()
+    except:
+        data_path = os.path.join(sys.executable.replace("\miniMoover.exe",""),"data")
+        print(data_path)
+        df_lista_macchine = dt.fread(os.path.join(data_path,"lista macchine.csv")).to_pandas()
     lista_info_macchine = df_lista_macchine.to_dict("records")
 except:
-    sys.exit(f"{Fore.RED}[ERRORE]: File 'lista macchine.csv' non trovato. Uscita forzata del sistema.{Style.RESET_ALL}")
+     print(f"[red bold][ERRORE]: File 'lista macchine.csv' non trovato. Uscita forzata del sistema.[/red bold]")
+     sys.exit()
 
 # Estrapolazione lista articoli su dict 'lista_articoli_possibili_full'
 lista_articoli_possibili_full = {}
 for file in os.scandir("data"):
-    if ".csv" in file.name and file.name != "lista macchine.csv":
-        tmp_df = pd.read_csv(f"data/{file.name}")
+    if ".csv" in file.name and file.name != "lista macchine.csv":    
+        tmp_df = dt.fread(f"{data_path}/{file.name}").to_pandas()
         for codmacchina in tmp_df["CODMACCHINA"].unique():
-            print_log(f"{Back.GREEN} Articoli per {codmacchina} aggiunti. {Style.RESET_ALL}")
+            print_log(f"[bright_green]Articoli per {codmacchina} aggiunti.[/bright_green]")
             lista_articoli_possibili_full[codmacchina] = tmp_df.to_dict("records")
 
 # Controllo file articoli mancanti (presente in lista macchine ma non ha associati articoli)
@@ -246,16 +208,16 @@ for index, riga in enumerate(lista_info_macchine):
     if codmacchina not in lista_articoli_possibili_full:
         continuare = "g"
         while(continuare != "S" and continuare != "N"):
-            continuare = input(f"{Back.YELLOW}ATTENZIONE: Nessun articolo trovato per {codmacchina}.\nContinuare con le altre? [S/N] {Style.RESET_ALL}").upper()
+            continuare = input(f"[yellow]ATTENZIONE: Nessun articolo trovato per {codmacchina}.\nContinuare con le altre? [S/N] [/yellow]").upper()
         match continuare:
             case "S":
-                print_log(f"{Back.YELLOW}L'elaborazione della macchina {codmacchina} verrà ignorata.{Style.RESET_ALL}")
+                print_log(f"[yellow]L'elaborazione della macchina {codmacchina} verrà ignorata.[/yellow]")
                 lista_info_macchine.pop(index)
             case "N":
-                sys.exit(f"{Fore.RED}[ARRESTO] Fermato dall'utente.{Style.RESET_ALL}")
+                sys.exit(f"[red][ARRESTO] Fermato dall'utente.[/red]")
 # TODO Inserire controllo completezza campi su lista_info_macchine
 # Tutte le macchine in lista_info_macchine sono processabili
-output, lista_ordini = [],[]
+lista_ordini = []
 # Senza questa riga le impostazioni di default rischiano di bloccare il ciclo di flat_to_dict()
 sys.setrecursionlimit(1600)
 for macchina in lista_info_macchine:
@@ -263,15 +225,31 @@ for macchina in lista_info_macchine:
 # -- Riformattazione
 print_log(f"Riformattazione lista ordini raw in righe separate... ")
 # Porta la lista in 2D
-lista_ordini = flat_to_dict(lista_ordini)
+with pro.Progress(pro.SpinnerColumn(style="bold rgb(1,185,255)"),
+                    pro.TextColumn("{task.description}",style="bold rgb(1,185,255)",),
+                    pro.BarColumn(pulse_style="dim rgb(1,185,255)",finished_style="bright_green"),
+                    pro.TimeElapsedColumn(),
+                    refresh_per_second=60) as waitingbar:
+    task = waitingbar.add_task("In esecuzione...",total=None)
+    lista_ordini = flat_to_dict(lista_ordini)
+    waitingbar.update(task, description="[bright_green]Completato!",total=1,completed=1)
 # Elimina la righe non lavorate
+print_log("Pulizia righe non lavorate...")
 tmp_lista_ordini = []
-for i,item in enumerate(lista_ordini):
-    if "fine lavorazione" in item:
-        tmp_lista_ordini.append(item)
-    print_log(f"{i+1}/{len(lista_ordini)} righe processate.",rewritable=True)
+with pro.Progress(pro.SpinnerColumn(style="bold rgb(1,185,255)"),
+                    pro.TextColumn("{task.description}",style="bold rgb(1,185,255)",),
+                    pro.BarColumn(style="dim rgb(1,185,255)",complete_style="bold rgb(1,185,255)", finished_style="bright_green"),
+                    pro.MofNCompleteColumn(),
+                    pro.TimeElapsedColumn(),
+                    refresh_per_second=60) as progressbar:
+    task = progressbar.add_task("In esecuzione...",total=len(lista_ordini))
+    for i,item in enumerate(lista_ordini):
+        if "fine lavorazione" in item:
+            tmp_lista_ordini.append(item)
+        progressbar.update(task, advance=1)
+    progressbar.update(task, description="[green]Completato![/green]")
 lista_ordini = tmp_lista_ordini
-print_log(f"{Back.GREEN} COMPLETATO {Style.RESET_ALL}")
+
 
 tmp_old_len_lista_articoli = len(lista_ordini)
 tmp_lista_ordini = []
@@ -281,40 +259,43 @@ print_log("Riordinamento delle righe in ordine cronologico...")
 df_righe = pd.DataFrame(lista_ordini)
 # -- Mischia le righe degli ordini considerando solo l'ordine cronologico
 df_righe.sort_values(by="fine lavorazione", inplace=True)
-print_log(f"{Back.GREEN} COMPLETATO {Style.RESET_ALL}")
 # -- Creazione codici NUMREG definitivi
 print_log("Creazione NUMREG condizionati.")
-print_log("Attendere...", rewritable=True)
 # -- Crea una tabella che fa da legenda per il cambio NUMREG
 tmp_list_numreg = df_righe.sort_values('inizio lavorazione').groupby('NUMREG').tail(1)[["NUMREG","fine lavorazione"]].copy(deep=True)
 list_new_numreg = []
 tmp_dates = pd.DatetimeIndex(tmp_list_numreg["fine lavorazione"].values)
-for x in range(0,len(tmp_list_numreg)): # LINK https://www.delftstack.com/howto/python/pad-string-with-zeros-in-python/
-    tmp_new_NUMREG = str(tmp_dates.year[x])
-    tmp_new_NUMREG = tmp_new_NUMREG.ljust(16-(len(str(x))+len(tmp_new_NUMREG)),"0")
-    tmp_new_NUMREG += str(x+1)
-    #list_new_numreg.append({"old NUMREG": tmp_list_numreg["NUMREG"].values[x], "new NUMREG":tmp_new_NUMREG})
-    df_righe.loc[df_righe["NUMREG"] == tmp_list_numreg["NUMREG"].values[x], "NUMREG"] = tmp_new_NUMREG
-    print_log(f"NUMREG creati e applicati: {x+1}/{len(tmp_list_numreg)}.",rewritable=True)
-print_log(f"{Back.GREEN} COMPLETATO {Style.RESET_ALL}")
+with pro.Progress(pro.SpinnerColumn(style="bold rgb(1,185,255)"),
+    pro.TextColumn("{task.description}",style="bold rgb(1,185,255)",),
+    pro.BarColumn(style="dim rgb(1,185,255)",complete_style="bold rgb(1,185,255)", finished_style="bright_green"),
+    pro.MofNCompleteColumn(),
+    pro.TimeElapsedColumn(),
+    refresh_per_second=60) as progressbar:
+        task = progressbar.add_task("In esecuzione...",total=len(tmp_list_numreg))
+        for x in range(0,len(tmp_list_numreg)): # LINK https://www.delftstack.com/howto/python/pad-string-with-zeros-in-python/
+            tmp_new_NUMREG = str(tmp_dates.year[x])
+            tmp_new_NUMREG = tmp_new_NUMREG.ljust(16-(len(str(x+1))+len(tmp_new_NUMREG)),"0")
+            tmp_new_NUMREG += str(x+1)
+            df_righe.loc[df_righe["NUMREG"] == tmp_list_numreg["NUMREG"].values[x], "NUMREG"] = tmp_new_NUMREG
+            progressbar.update(task, advance=1)
+        progressbar.update(task, description="[green]Completato![/green]")
 
 # -- Processo di output
 print_log("Creazione CSV per INSERT...")
-print_log("Attendere...", rewritable=True)
 lista_righe = df_righe.to_dict(orient="list")
 tmp_num_righe = len(lista_righe["NUMREG"])
 
 # BACKUP LISTA IN CASO DI ERRORE IN SEGUITO
-print_log(" |__ Salvataggio per backup come disaster recovery.")
+print_log("Salvataggio file temporaneo di output per backup.")
 save_var("lista_righe", lista_righe)
 
 # Conversione date
-print_log(" |__ Cambio nomi colonne e conversione date.")
+print_log("Conversione date formato date.")
 tmp_campi_time=["fine lavorazione", "inizio lavorazione", "data consolidamento"]
 for x in tmp_campi_time:
     for i,y in enumerate(lista_righe[x]):
         lista_righe[x][i] = y.to_pydatetime().strftime("%Y-%m-%d %H:%M:%S")
-    print_log(f"   |__ Campo '{x}' convertito.")
+    print_log(f"Campo '{x}' convertito.")
 
 # Sistemazione Colonne
 lista_righe["DITTA"] = [1] * tmp_num_righe
@@ -341,5 +322,10 @@ a = dt.Frame(lista_righe)
 
 # Salvataggio
 a.to_csv(to_desktop("output.csv"))
-print_log(f"{Fore.GREEN}Creato file 'output.csv' su Desktop.{Style.RESET_ALL}")
-print_log(f"{Fore.GREEN}{tmp_num_righe} righe processate in {datetime.now()-script_start_time}.{Style.RESET_ALL}")
+print_log(f"[bright_green]Creato file 'output.csv' su Desktop.[/bright_green]")
+print_log(f"[bright_green]{tmp_num_righe} righe processate in {datetime.now()-script_start_time}.[/bright_green]")
+try:
+    os.system("pause")
+except:
+    input("Premere Enter per terminare...")
+    
